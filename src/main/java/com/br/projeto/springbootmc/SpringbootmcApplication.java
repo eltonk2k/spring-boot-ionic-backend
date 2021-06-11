c1ea7545bec1a6d7dc9aa231f79895d6c8ff7b05
Elielton Silva <elieltonpb1@gmail.com>
2021-06-11 10:24:56 -0300

    Primeiro commit do projeto

---
+++ src/main/java/com/br/projeto/springbootmc/SpringbootmcApplication.java
@@ -0,0 +1,13 @@
+package com.br.projeto.springbootmc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootmcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootmcApplication.class, args);
+	}
+
+}
