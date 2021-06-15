d1ba6c110c5c3b82fb88ae1e5a8f6acfc6211073
eltonk2k <elieltonpb1@gmail.com>
2021-06-14 21:09:56 -0300

    Endpoint /pedidos/{id} disponibilizado

--- src/main/java/com/br/projeto/springbootmc/domain/Pedido.java
+++ src/main/java/com/br/projeto/springbootmc/domain/Pedido.java
@@ -15,6 +15,9 @@ import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
 @Entity
 public class Pedido implements Serializable {
 	private static final long serialVersionUID = 1L;
@@ -22,11 +25,15 @@ public class Pedido implements Serializable {
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
-	private Date data;
 	
+	@JsonFormat(pattern="dd/MM/yyyy HH:mm")
+	private Date instante;
+	
+	@JsonManagedReference
 	@OneToOne(cascade=CascadeType.ALL, mappedBy="pedido")
 	private Pagamento pagamento;
 	
+	@JsonManagedReference
 	@ManyToOne
 	@JoinColumn(name="cliente_id")
 	private Cliente cliente;
@@ -41,10 +48,10 @@ public class Pedido implements Serializable {
 	public Pedido() {
 	}
 
-	public Pedido(Integer id, Date data, Cliente cliente, Endereco enderecoDeEntrega) {
+	public Pedido(Integer id, Date instante, Cliente cliente, Endereco enderecoDeEntrega) {
 		super();
 		this.id = id;
-		this.data = data;
+		this.instante = instante;
 		this.cliente = cliente;
 		this.enderecoDeEntrega = enderecoDeEntrega;
 	}
@@ -58,11 +65,11 @@ public class Pedido implements Serializable {
 	}
 
 	public Date getData() {
-		return data;
+		return instante;
 	}
 
-	public void setData(Date data) {
-		this.data = data;
+	public void setData(Date instante) {
+		this.instante = instante;
 	}
 
 	public Pagamento getPagamento() {
