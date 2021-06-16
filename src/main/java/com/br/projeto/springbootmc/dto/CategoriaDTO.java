9233c44820205d3356a1c9405fe12e23c55c27ad
eltonk2k <elieltonpb1@gmail.com>
2021-06-16 09:43:44 -0300

    Listando todas categorias com DTO

---
+++ src/main/java/com/br/projeto/springbootmc/dto/CategoriaDTO.java
@@ -0,0 +1,39 @@
+package com.br.projeto.springbootmc.dto;
+
+import java.io.Serializable;
+
+import com.br.projeto.springbootmc.domain.Categoria;
+
+public class CategoriaDTO implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id;
+	private String nome;
+	
+	public CategoriaDTO() {
+	}
+	
+	public CategoriaDTO(Categoria obj) {
+		id = obj.getId();
+		nome = obj.getNome();
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+
+}
--- src/main/java/com/br/projeto/springbootmc/resources/CategoriaResource.java
+++ src/main/java/com/br/projeto/springbootmc/resources/CategoriaResource.java
@@ -1,6 +1,8 @@
 package com.br.projeto.springbootmc.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.br.projeto.springbootmc.domain.Categoria;
+import com.br.projeto.springbootmc.dto.CategoriaDTO;
 import com.br.projeto.springbootmc.services.CategoriaService;
 
 @RestController
@@ -45,6 +48,13 @@ public class CategoriaResource {
 	public ResponseEntity<Void> delete(@PathVariable Integer id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();		
+	}
+
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<Categoria> list = service.findAll();
+		List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	}
 
 }
