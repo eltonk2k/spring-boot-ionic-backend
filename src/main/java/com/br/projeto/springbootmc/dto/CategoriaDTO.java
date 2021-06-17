f77de0599267bedd3c02c72621f9a6a3b978e6f4
eltonk2k <elieltonpb1@gmail.com>
2021-06-16 21:24:55 -0300

    Validação sintática com Bean Validation

--- src/main/java/com/br/projeto/springbootmc/dto/CategoriaDTO.java
+++ src/main/java/com/br/projeto/springbootmc/dto/CategoriaDTO.java
@@ -3,11 +3,17 @@ package com.br.projeto.springbootmc.dto;
 import java.io.Serializable;
 
 import com.br.projeto.springbootmc.domain.Categoria;
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
 
 public class CategoriaDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	private Integer id;
+	
+	@NotEmpty(message="Preenchimento Obrigatório")
+	@Length(min=5, max=80, message="O tamanho deve ser entre 5 e 80 caracteres")
 	private String nome;
 	
 	public CategoriaDTO() {
@@ -33,7 +39,5 @@ public class CategoriaDTO implements Serializable {
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-	
-	
 
 }
