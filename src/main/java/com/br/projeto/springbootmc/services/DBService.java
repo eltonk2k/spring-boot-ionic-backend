fcb0282ffb91f57aaf985295adac2b76885113ea
eltonk2k <elieltonpb1@gmail.com>
2021-06-26 21:58:22 -0300

    Salvando perfis de usuario na base de dados

--- src/main/java/com/br/projeto/springbootmc/services/DBService.java
+++ src/main/java/com/br/projeto/springbootmc/services/DBService.java
@@ -20,6 +20,7 @@ import com.br.projeto.springbootmc.domain.PagamentoComCartao;
 import com.br.projeto.springbootmc.domain.Pedido;
 import com.br.projeto.springbootmc.domain.Produto;
 import com.br.projeto.springbootmc.domain.enums.EstadoPagamento;
+import com.br.projeto.springbootmc.domain.enums.Perfil;
 import com.br.projeto.springbootmc.domain.enums.TipoCliente;
 import com.br.projeto.springbootmc.repositories.CategoriaRepository;
 import com.br.projeto.springbootmc.repositories.CidadeRepository;
@@ -124,16 +125,22 @@ public class DBService {
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 		
 		Cliente cli1 = new Cliente(null, "Maria Silva", "pbexecutores2@gmail.com", "36378912377", TipoCliente.PESSOAFISICA, pe.encode("123"));
-		
 		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
+		
+		Cliente cli2 = new Cliente(null, "Elton Silva", "pbexecutores@gmail.com", "27524278080", TipoCliente.PESSOAFISICA, pe.encode("123"));
+		cli2.getTelefones().addAll(Arrays.asList("98754515", "32323232"));
+		cli2.addPerfil(Perfil.ADMIN);
+		
 		
 		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardim", "38220834", cli1, c1);
 		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
+		Endereco e3 = new Endereco(null, "Avenida Floriana Costa", "2001", null, "Centro", "58777012", cli2, c2);
 		
 		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+		cli2.getEnderecos().addAll(Arrays.asList(e3));
 		
-		clienteRepository.saveAll(Arrays.asList(cli1));
-		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+		clienteRepository.saveAll(Arrays.asList(cli1, cli2));
+		enderecoRepository.saveAll(Arrays.asList(e1, e2, e3));
 		
 		SimpleDateFormat sdf  = new SimpleDateFormat("dd/MM/yyyy HH:mm");
 		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
