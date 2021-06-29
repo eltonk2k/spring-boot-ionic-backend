016858693edabdf0a463a7432d62b130691b1b88
eltonk2k <elieltonpb1@gmail.com>
2021-06-28 23:20:25 -0300

    Implementando autorização

--- src/main/java/com/br/projeto/springbootmc/config/SecurityConfig.java
+++ src/main/java/com/br/projeto/springbootmc/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.br.projeto.springbootmc.security.JWTAuthenticationFilter;
+import com.br.projeto.springbootmc.security.JWTAuthorizationFilter;
 import com.br.projeto.springbootmc.security.JWTUtil;
 
 @Configuration
@@ -55,6 +56,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.antMatchers(PUBLIC_MATCHERS).permitAll()
 			.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
