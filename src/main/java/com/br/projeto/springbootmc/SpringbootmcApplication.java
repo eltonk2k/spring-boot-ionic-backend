b9f21a11599cf512c6e737e5b4ecd303d6b79e0d
eltonk2k <elieltonpb1@gmail.com>
2021-07-02 22:51:49 -0300

    Salvando primeiro arquivo no S3

--- src/main/java/com/br/projeto/springbootmc/SpringbootmcApplication.java
+++ src/main/java/com/br/projeto/springbootmc/SpringbootmcApplication.java
@@ -1,13 +1,17 @@
 package com.br.projeto.springbootmc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.br.projeto.springbootmc.services.S3Service;
+
 @SpringBootApplication
 public class SpringbootmcApplication implements CommandLineRunner {
 
-	
+	@Autowired
+	private S3Service s3Service;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootmcApplication.class, args);
@@ -15,7 +19,7 @@ public class SpringbootmcApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		
+		s3Service.uploadFile("D:\\Documentos\\Lightshot\\requisitos.png");
 		
 	}
 
